49f4b4e15218e7893a5bf4d82c050c0fca4e1287
ISpectruM <ispectrumcode@gmail.com>
2018-01-07 03:04:50 +0200

    Prevented a rating submit when rating star is not selected.

--- TravelGuide/src/main/resources/static/scripts/votingStars.js
+++ TravelGuide/src/main/resources/static/scripts/votingStars.js
@@ -1,23 +1,26 @@
-$(()=>{
+$(() => {
+    let button = $("input[type='submit']");
+
     $('.rating')
         .addClass('starRating')
-        .on('mouseenter', 'label', function(){
+        .on('mouseenter', 'label', function () {
                 DisplayRating($(this)); // show the ratings on label hover
             }
         )
-        .on('mouseleave', function() {
-                // when we leave the rating div, figure out which one is selected and show the correct rating level
-                let $this = $(this),
-                    $selectedRating = $this.find('input:checked');
+        .on('mouseleave', function () {
+                let $this = $(this);
+                let $selectedRating = $this.find('input:checked');
                 if ($selectedRating.length === 1) {
                     DisplayRating($selectedRating); // a rating has been selected, show the stars
+                    button.prop('disabled', false);
                 } else {
                     $this.find('label').removeClass('on'); // nothing clicked, remove the stars
+                    button.prop('disabled', true);
                 }
             }
         );
 
-    let DisplayRating = function($el){
+    let DisplayRating = function ($el) {
         // for the passed in element, add the 'on' class to this and all prev labels
         // and remove the 'on' class from all next labels. This stops the flicker of removing then adding back
         $el.addClass('on');
